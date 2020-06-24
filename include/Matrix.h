419a4a81bba4e267c4812f4d05fdb2330d17f94a
Rhythm Chopra <rhythmchopra8@gmail.com>
2020-06-24 10:25:06 +0300

    Create Matrix using vector of vectors
    Override extraction operator

--- include/Matrix.h
+++ include/Matrix.h
@@ -5,11 +5,20 @@
 #ifndef MATRIX_AND_VECTOR_MATRIX_H
 #define MATRIX_AND_VECTOR_MATRIX_H
 
+#include <vector>
+
 class Matrix {
 public:
-    Matrix();
+    Matrix(int rows, int cols, int val);
+
+    int getCols();
+    int getRows();
+
+    friend std::ostream &operator<<( std::ostream &output, const Matrix &m );
 
 private:
+    int rows, cols;
+    std::vector<std::vector<int>> data;
 };
 
 #endif //MATRIX_AND_VECTOR_MATRIX_H
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -4,6 +4,33 @@
 #include <iostream>
 #include <Matrix.h>
 
-Matrix::Matrix() {
-    std::cout << "Creating Matrix" << std::endl;
+Matrix::Matrix(int rows, int cols, int val) {
+    this->rows = rows;
+    this->cols = cols;
+    for (int i = 0; i < this->rows; i++) {
+        std::vector<int> temp;
+        for (int j = 0; j < this->cols; j++) {
+            temp.emplace_back(val);
+        }
+        this->data.emplace_back(temp);
+    }
+}
+
+int Matrix::getCols() {
+    return this->cols;
+}
+
+int Matrix::getRows() {
+    return this->rows;
+}
+
+std::ostream &operator<<(std::ostream &output, const Matrix &m) {
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            output << m.data[i][j] << "  ";
+        }
+        output << std::endl;
+    }
+    output << std::endl;
+    return output;
 }

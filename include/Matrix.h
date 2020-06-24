c443d7c3af441df3854a036d3221201a25fc14d1
Rhythm Chopra <rhythmchopra8@gmail.com>
2020-06-24 11:27:49 +0300

    Matrix arithmetic operations added

--- include/Matrix.h
+++ include/Matrix.h
@@ -6,15 +6,23 @@
 #define MATRIX_AND_VECTOR_MATRIX_H
 
 #include <vector>
+#include <memory>
 
 class Matrix {
 public:
     Matrix(int rows, int cols, int val);
 
-    int getCols();
-    int getRows();
+    int getCols() const;
 
-    friend std::ostream &operator<<( std::ostream &output, const Matrix &m );
+    int getRows() const;
+
+    friend std::ostream &operator<<(std::ostream &output, const Matrix &m);
+
+    Matrix operator+(const Matrix &m);
+
+    Matrix operator*(int factor);
+
+    Matrix operator*(const Matrix &m);
 
 private:
     int rows, cols;
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -16,21 +16,53 @@ Matrix::Matrix(int rows, int cols, int val) {
     }
 }
 
-int Matrix::getCols() {
+int Matrix::getCols() const {
     return this->cols;
 }
 
-int Matrix::getRows() {
+int Matrix::getRows() const {
     return this->rows;
 }
 
 std::ostream &operator<<(std::ostream &output, const Matrix &m) {
-    for (int i = 0; i < m.rows; i++) {
-        for (int j = 0; j < m.cols; j++) {
+    for (int i = 0; i < m.getRows(); i++) {
+        for (int j = 0; j < m.getCols(); j++) {
             output << m.data[i][j] << "  ";
         }
         output << std::endl;
     }
     output << std::endl;
     return output;
+}
+
+Matrix Matrix::operator+(const Matrix &m) {
+    if (this->rows != m.getRows() && this->cols != m.getCols())
+        throw std::range_error("Dimensions mismatch for matrix addition !!!");
+    Matrix result = Matrix(this->rows, this->cols, 0);
+    for (int i = 0; i < this->rows; i++)
+        for (int j = 0; j < this->cols; j++)
+            result.data[i][j] = this->data[i][j] + m.data[i][j];
+    return result;
+}
+
+Matrix Matrix::operator*(int factor) {
+    Matrix result = Matrix(this->rows, this->cols, 0);
+    for (int i = 0; i < this->rows; i++)
+        for (int j = 0; j < this->cols; j++)
+            result.data[i][j] = factor * this->data[i][j];
+    return result;
+}
+
+Matrix Matrix::operator*(const Matrix &m) {
+    if (this->cols != m.getRows())
+        throw std::range_error("Dimensions mismatch for matrix multiplication !!!");
+    Matrix result = Matrix(this->rows, m.getCols(), 0);
+    for (int i = 0; i < this->rows; i++)
+        for (int j = 0; j < m.getCols(); j++) {
+            int temp = 0;
+            for (int k = 0; k < this->cols; k++)
+                temp += (this->data[i][k] * m.data[k][j]);
+            result.data[i][j] = temp;
+        }
+    return result;
 }
